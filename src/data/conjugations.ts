351018d34f340a1d3f02f712ba23bc73a8300778
Snail Turbo <snail.mailer2022@gmail.com>
2026-02-10 22:32:09 -0500

    Feat: Add final batch of common irregular verbs

--- src/data/conjugations.ts
+++ src/data/conjugations.ts
@@ -296,5 +296,292 @@ export const verbs: VerbDefinition[] = [
                 { pronoun: 'ils/elles', form: 'voudront' }
             ]
         }
+    },
+    {
+        infinitive: 'savoir',
+        translation: 'to know',
+        group: 3,
+        auxiliary: 'avoir',
+        rules: ['Irregular.', 'Participle: su.'],
+        conjugations: {
+            'Présent': [
+                { pronoun: 'je', form: 'sais' },
+                { pronoun: 'tu', form: 'sais' },
+                { pronoun: 'il/elle', form: 'sait' },
+                { pronoun: 'nous', form: 'savons' },
+                { pronoun: 'vous', form: 'savez' },
+                { pronoun: 'ils/elles', form: 'savent' }
+            ],
+            'Passé Composé': [
+                { pronoun: 'j\'', form: 'ai su' },
+                { pronoun: 'tu', form: 'as su' },
+                { pronoun: 'il/elle', form: 'a su' },
+                { pronoun: 'nous', form: 'avons su' },
+                { pronoun: 'vous', form: 'avez su' },
+                { pronoun: 'ils/elles', form: 'ont su' }
+            ],
+            'Imparfait': [
+                { pronoun: 'je', form: 'savais' },
+                { pronoun: 'tu', form: 'savais' },
+                { pronoun: 'il/elle', form: 'savait' },
+                { pronoun: 'nous', form: 'savions' },
+                { pronoun: 'vous', form: 'saviez' },
+                { pronoun: 'ils/elles', form: 'savaient' }
+            ],
+            'Futur Simple': [
+                { pronoun: 'je', form: 'saurai' },
+                { pronoun: 'tu', form: 'sauras' },
+                { pronoun: 'il/elle', form: 'saura' },
+                { pronoun: 'nous', form: 'saurons' },
+                { pronoun: 'vous', form: 'saurez' },
+                { pronoun: 'ils/elles', form: 'sauront' }
+            ]
+        }
+    },
+    {
+        infinitive: 'devoir',
+        translation: 'to have to (must)',
+        group: 3,
+        auxiliary: 'avoir',
+        rules: ['Irregular.', 'Participle: dû.'],
+        conjugations: {
+            'Présent': [
+                { pronoun: 'je', form: 'dois' },
+                { pronoun: 'tu', form: 'dois' },
+                { pronoun: 'il/elle', form: 'doit' },
+                { pronoun: 'nous', form: 'devons' },
+                { pronoun: 'vous', form: 'devez' },
+                { pronoun: 'ils/elles', form: 'doivent' }
+            ],
+            'Passé Composé': [
+                { pronoun: 'j\'', form: 'ai dû' },
+                { pronoun: 'tu', form: 'as dû' },
+                { pronoun: 'il/elle', form: 'a dû' },
+                { pronoun: 'nous', form: 'avons dû' },
+                { pronoun: 'vous', form: 'avez dû' },
+                { pronoun: 'ils/elles', form: 'ont dû' }
+            ],
+            'Imparfait': [
+                { pronoun: 'je', form: 'devais' },
+                { pronoun: 'tu', form: 'devais' },
+                { pronoun: 'il/elle', form: 'devait' },
+                { pronoun: 'nous', form: 'devions' },
+                { pronoun: 'vous', form: 'deviez' },
+                { pronoun: 'ils/elles', form: 'devaient' }
+            ],
+            'Futur Simple': [
+                { pronoun: 'je', form: 'devrai' },
+                { pronoun: 'tu', form: 'devras' },
+                { pronoun: 'il/elle', form: 'devra' },
+                { pronoun: 'nous', form: 'devrons' },
+                { pronoun: 'vous', form: 'devrez' },
+                { pronoun: 'ils/elles', form: 'devront' }
+            ]
+        }
+    },
+    {
+        infinitive: 'prendre',
+        translation: 'to take',
+        group: 3,
+        auxiliary: 'avoir',
+        rules: ['Irregular.', 'Participle: pris.'],
+        conjugations: {
+            'Présent': [
+                { pronoun: 'je', form: 'prends' },
+                { pronoun: 'tu', form: 'prends' },
+                { pronoun: 'il/elle', form: 'prend' },
+                { pronoun: 'nous', form: 'prenons' },
+                { pronoun: 'vous', form: 'prenez' },
+                { pronoun: 'ils/elles', form: 'prennent' }
+            ],
+            'Passé Composé': [
+                { pronoun: 'j\'', form: 'ai pris' },
+                { pronoun: 'tu', form: 'as pris' },
+                { pronoun: 'il/elle', form: 'a pris' },
+                { pronoun: 'nous', form: 'avons pris' },
+                { pronoun: 'vous', form: 'avez pris' },
+                { pronoun: 'ils/elles', form: 'ont pris' }
+            ],
+            'Imparfait': [
+                { pronoun: 'je', form: 'prenais' },
+                { pronoun: 'tu', form: 'prenais' },
+                { pronoun: 'il/elle', form: 'prenait' },
+                { pronoun: 'nous', form: 'prenions' },
+                { pronoun: 'vous', form: 'preniez' },
+                { pronoun: 'ils/elles', form: 'prenaient' }
+            ],
+            'Futur Simple': [
+                { pronoun: 'je', form: 'prendrai' },
+                { pronoun: 'tu', form: 'prendras' },
+                { pronoun: 'il/elle', form: 'prendra' },
+                { pronoun: 'nous', form: 'prendrons' },
+                { pronoun: 'vous', form: 'prendrez' },
+                { pronoun: 'ils/elles', form: 'prendront' }
+            ]
+        }
+    },
+    {
+        infinitive: 'mettre',
+        translation: 'to put',
+        group: 3,
+        auxiliary: 'avoir',
+        rules: ['Irregular.', 'Participle: mis.'],
+        conjugations: {
+            'Présent': [
+                { pronoun: 'je', form: 'mets' },
+                { pronoun: 'tu', form: 'mets' },
+                { pronoun: 'il/elle', form: 'met' },
+                { pronoun: 'nous', form: 'mettons' },
+                { pronoun: 'vous', form: 'mettez' },
+                { pronoun: 'ils/elles', form: 'mettent' }
+            ],
+            'Passé Composé': [
+                { pronoun: 'j\'', form: 'ai mis' },
+                { pronoun: 'tu', form: 'as mis' },
+                { pronoun: 'il/elle', form: 'a mis' },
+                { pronoun: 'nous', form: 'avons mis' },
+                { pronoun: 'vous', form: 'avez mis' },
+                { pronoun: 'ils/elles', form: 'ont mis' }
+            ],
+            'Imparfait': [
+                { pronoun: 'je', form: 'mettais' },
+                { pronoun: 'tu', form: 'mettais' },
+                { pronoun: 'il/elle', form: 'mettait' },
+                { pronoun: 'nous', form: 'mettions' },
+                { pronoun: 'vous', form: 'mettiez' },
+                { pronoun: 'ils/elles', form: 'mettaient' }
+            ],
+            'Futur Simple': [
+                { pronoun: 'je', form: 'mettrai' },
+                { pronoun: 'tu', form: 'mettras' },
+                { pronoun: 'il/elle', form: 'mettra' },
+                { pronoun: 'nous', form: 'mettrons' },
+                { pronoun: 'vous', form: 'mettrez' },
+                { pronoun: 'ils/elles', form: 'mettront' }
+            ]
+        }
+    },
+    {
+        infinitive: 'voir',
+        translation: 'to see',
+        group: 3,
+        auxiliary: 'avoir',
+        rules: ['Irregular.', 'Participle: vu.', 'Future stem: verr-'],
+        conjugations: {
+            'Présent': [
+                { pronoun: 'je', form: 'vois' },
+                { pronoun: 'tu', form: 'vois' },
+                { pronoun: 'il/elle', form: 'voit' },
+                { pronoun: 'nous', form: 'voyons' },
+                { pronoun: 'vous', form: 'voyez' },
+                { pronoun: 'ils/elles', form: 'voient' }
+            ],
+            'Passé Composé': [
+                { pronoun: 'j\'', form: 'ai vu' },
+                { pronoun: 'tu', form: 'as vu' },
+                { pronoun: 'il/elle', form: 'a vu' },
+                { pronoun: 'nous', form: 'avons vu' },
+                { pronoun: 'vous', form: 'avez vu' },
+                { pronoun: 'ils/elles', form: 'ont vu' }
+            ],
+            'Imparfait': [
+                { pronoun: 'je', form: 'voyais' },
+                { pronoun: 'tu', form: 'voyais' },
+                { pronoun: 'il/elle', form: 'voyait' },
+                { pronoun: 'nous', form: 'voyions' },
+                { pronoun: 'vous', form: 'voyiez' },
+                { pronoun: 'ils/elles', form: 'voyaient' }
+            ],
+            'Futur Simple': [
+                { pronoun: 'je', form: 'verrai' },
+                { pronoun: 'tu', form: 'verras' },
+                { pronoun: 'il/elle', form: 'verra' },
+                { pronoun: 'nous', form: 'verrons' },
+                { pronoun: 'vous', form: 'verrez' },
+                { pronoun: 'ils/elles', form: 'verront' }
+            ]
+        }
+    },
+    {
+        infinitive: 'dire',
+        translation: 'to say',
+        group: 3,
+        auxiliary: 'avoir',
+        rules: ['Irregular.', 'Note "vous dites" in present.', 'Participle: dit.'],
+        conjugations: {
+            'Présent': [
+                { pronoun: 'je', form: 'dis' },
+                { pronoun: 'tu', form: 'dis' },
+                { pronoun: 'il/elle', form: 'dit' },
+                { pronoun: 'nous', form: 'disons' },
+                { pronoun: 'vous', form: 'dites' },
+                { pronoun: 'ils/elles', form: 'disent' }
+            ],
+            'Passé Composé': [
+                { pronoun: 'j\'', form: 'ai dit' },
+                { pronoun: 'tu', form: 'as dit' },
+                { pronoun: 'il/elle', form: 'a dit' },
+                { pronoun: 'nous', form: 'avons dit' },
+                { pronoun: 'vous', form: 'avez dit' },
+                { pronoun: 'ils/elles', form: 'ont dit' }
+            ],
+            'Imparfait': [
+                { pronoun: 'je', form: 'disais' },
+                { pronoun: 'tu', form: 'disais' },
+                { pronoun: 'il/elle', form: 'disait' },
+                { pronoun: 'nous', form: 'disions' },
+                { pronoun: 'vous', form: 'disiez' },
+                { pronoun: 'ils/elles', form: 'disaient' }
+            ],
+            'Futur Simple': [
+                { pronoun: 'je', form: 'dirai' },
+                { pronoun: 'tu', form: 'diras' },
+                { pronoun: 'il/elle', form: 'dira' },
+                { pronoun: 'nous', form: 'dirons' },
+                { pronoun: 'vous', form: 'direz' },
+                { pronoun: 'ils/elles', form: 'diront' }
+            ]
+        }
+    },
+    {
+        infinitive: 'venir',
+        translation: 'to come',
+        group: 3,
+        auxiliary: 'être',
+        rules: ['Irregular.', 'Takes "être".', 'Future stem: viendr-'],
+        conjugations: {
+            'Présent': [
+                { pronoun: 'je', form: 'viens' },
+                { pronoun: 'tu', form: 'viens' },
+                { pronoun: 'il/elle', form: 'vient' },
+                { pronoun: 'nous', form: 'venons' },
+                { pronoun: 'vous', form: 'venez' },
+                { pronoun: 'ils/elles', form: 'viennent' }
+            ],
+            'Passé Composé': [
+                { pronoun: 'je', form: 'suis venu(e)' },
+                { pronoun: 'tu', form: 'es venu(e)' },
+                { pronoun: 'il/elle', form: 'est venu(e)' },
+                { pronoun: 'nous', form: 'sommes venus(es)' },
+                { pronoun: 'vous', form: 'êtes venus(es)' },
+                { pronoun: 'ils/elles', form: 'sont venus(es)' }
+            ],
+            'Imparfait': [
+                { pronoun: 'je', form: 'venais' },
+                { pronoun: 'tu', form: 'venais' },
+                { pronoun: 'il/elle', form: 'venait' },
+                { pronoun: 'nous', form: 'venions' },
+                { pronoun: 'vous', form: 'veniez' },
+                { pronoun: 'ils/elles', form: 'venaient' }
+            ],
+            'Futur Simple': [
+                { pronoun: 'je', form: 'viendrai' },
+                { pronoun: 'tu', form: 'viendras' },
+                { pronoun: 'il/elle', form: 'viendra' },
+                { pronoun: 'nous', form: 'viendrons' },
+                { pronoun: 'vous', form: 'viendrez' },
+                { pronoun: 'ils/elles', form: 'viendront' }
+            ]
+        }
     }
 ];
