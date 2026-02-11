d9aa1b704a593897026e097d5e9fe2762235f9c7
Snail Turbo <snail.mailer2022@gmail.com>
2026-02-10 21:35:35 -0500

    Fix: add base path and switch to HashRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import LandingPage from './pages/LandingPage';
 import LevelPage from './pages/LevelPage';
